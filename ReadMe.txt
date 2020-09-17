e6c4e9ed59718d653e79d848ef059ecbd5b1e508
Kurt Khris <KurtKhris>
2020-09-17 20:20:40 +0000

    Using the DOM element in Javascript

---
+++ ReadMe.txt
@@ -0,0 +1,40 @@
+Keyboard Key Assignments
+
+` == C#
+1 == D#
+2 == F#
+3 == G#
+4 == Bb
+5 == C#1
+6 == D#1
+7 == F#1
+8 == G#1
+9 == Bb1
+0 == C#2
+- == D#2
+= == F#2
+Backspace == G#2
+\ == Bb2
+
+Q == NOTE C
+W == NOTE D
+E == NOTE E
+R == NOTE F
+T == NOTE G
+Y == NOTE A
+U == NOTE B
+I == NOTE C1
+O == NOTE D1
+P == NOTE E1
+A == NOTE F1
+S == NOTE G1
+D == NOTE A1
+F == NOTE B1
+G == NOTE C2
+H == NOTE D2
+J == NOTE E2
+K == NOTE F2
+L == NOTE G2
+C == NOTE A2
+V == NOTE B2
+B == NOTE C3
---
+++ script.js
@@ -0,0 +1,533 @@
+var C = document.getElementById('C');
+var Cs = document.getElementById('Cs');
+var D = document.getElementById('D');
+var Ds = document.getElementById('Ds');
+var E = document.getElementById('E');
+var F = document.getElementById('F');
+var Fs = document.getElementById('Fs');
+var G = document.getElementById('G');
+var Gs = document.getElementById('Gs');
+var A = document.getElementById('A');
+var B = document.getElementById('B');
+var Bb = document.getElementById('Bb');
+var C1 = document.getElementById('C1');
+var Cs1 = document.getElementById('Cs1');
+var D1 = document.getElementById('D1');
+var Ds1 = document.getElementById('Ds1');
+var E1 = document.getElementById('E1');
+var F1 = document.getElementById('F1');
+var Fs1 = document.getElementById('Fs1');
+var G1 = document.getElementById('G1');
+var Gs1 = document.getElementById('Gs1');
+var A1 = document.getElementById('A1');
+var B1 = document.getElementById('B1');
+var Bb1 = document.getElementById('Bb1');
+var C2 = document.getElementById('C2');
+var Cs2 = document.getElementById('Cs2');
+var D2 = document.getElementById('D2');
+var Ds2 = document.getElementById('Ds2');
+var E2 = document.getElementById('E2');
+var F2 = document.getElementById('F2');
+var Fs2 = document.getElementById('Fs2');
+var G2 = document.getElementById('G2');
+var Gs2 = document.getElementById('Gs2');
+var A2 = document.getElementById('A2');
+var B2 = document.getElementById('B2');
+var Bb2 = document.getElementById('Bb2');
+var C3 = document.getElementById('C3');
+
+  //Events
+  C.addEventListener('click', ()=>{
+  	var myaudio = new Audio('C.wav');
+  	myaudio.play();
+  });
+
+  Cs.addEventListener('click', ()=>{
+  	var myaudio = new Audio('C_s.wav');
+  	myaudio.play();
+  });
+
+  D.addEventListener('click', ()=>{
+  	var myaudio = new Audio('D.wav');
+  	myaudio.play();
+  });
+
+  Ds.addEventListener('click', ()=>{
+  	var myaudio = new Audio('D_s.wav');
+  	myaudio.play();
+  });
+
+  E.addEventListener('click', ()=>{
+  	var myaudio = new Audio('E.wav');
+  	myaudio.play();
+  });
+
+  F.addEventListener('click', ()=>{
+  	var myaudio = new Audio('F.wav');
+  	myaudio.play();
+  });
+
+  Fs.addEventListener('click', ()=>{
+  	var myaudio = new Audio('F_s.wav');
+  	myaudio.play();
+  });
+
+  G.addEventListener('click', ()=>{
+  	var myaudio = new Audio('G.wav');
+  	myaudio.play();
+  });
+
+  Gs.addEventListener('click', ()=>{
+  	var myaudio = new Audio('G_s.wav');
+  	myaudio.play();
+  });
+
+  A.addEventListener('click', ()=>{
+  	var myaudio = new Audio('A.wav');
+  	myaudio.play();
+  });
+
+  B.addEventListener('click', ()=>{
+  	var myaudio = new Audio('B.wav');
+  	myaudio.play();
+  });
+
+  Bb.addEventListener('click', ()=>{
+  	var myaudio = new Audio('Bb.wav');
+  	myaudio.play();
+  });
+
+//Events2
+  C1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('C1.wav');
+  	myaudio.play();
+  });
+
+  Cs1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('C_s1.wav');
+  	myaudio.play();
+  });
+
+  D1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('D1.wav');
+  	myaudio.play();
+  });
+
+  Ds1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('D_s1.wav');
+  	myaudio.play();
+  });
+
+  E1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('E1.wav');
+  	myaudio.play();
+  });
+
+  F1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('F1.wav');
+  	myaudio.play();
+  });
+
+  Fs1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano17.mp3');
+  	myaudio.play();
+  });
+
+  G1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano120.mp3');
+  	myaudio.play();
+  });
+
+  Gs1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano121.mp3');
+  	myaudio.play();
+  });
+
+  A1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano110.mp3');
+  	myaudio.play();
+  });
+
+  B1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano112.mp3');
+  	myaudio.play();
+  });
+
+  Bb1.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano111.mp3');
+  	myaudio.play();
+  });
+
+
+  //Events3
+  C2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano125.mp3');
+  	myaudio.play();
+  });
+
+  Cs2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano114.mp3');
+  	myaudio.play();
+  });
+
+  D2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano115.mp3');
+  	myaudio.play();
+  });
+
+  Ds2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano116.mp3');
+  	myaudio.play();
+  });
+
+  E2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano117.mp3');
+  	myaudio.play();
+  });
+
+  F2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano118.mp3');
+  	myaudio.play();
+  });
+
+  Fs2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano119.mp3');
+  	myaudio.play();
+  });
+
+  G2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano18.mp3');
+  	myaudio.play();
+  });
+
+  Gs2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano19.mp3');
+  	myaudio.play();
+  });
+
+  A2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano122.mp3');
+  	myaudio.play();
+  });
+
+  B2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano124.mp3');
+  	myaudio.play();
+  });
+
+  Bb2.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano123.mp3');
+  	myaudio.play();
+  });
+
+   C3.addEventListener('click', ()=>{
+  	var myaudio = new Audio('./audios/Piano113.mp3');
+  	myaudio.play();
+  });
+
+
+  //Keyboard Keys
+
+  var keypressed = document.body;
+
+  keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 81){
+  		var myaudio = new Audio('C.wav');
+  		myaudio.play();
+  	}
+  })
+
+   keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 192){
+  		var myaudio = new Audio('C_s.wav');
+  		myaudio.play();
+  	}
+  })
+
+
+  keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 87){
+  		var myaudio = new Audio('D.wav');
+  		myaudio.play();
+  	}
+  })
+
+  keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 49){
+  		var myaudio = new Audio('D_s.wav');
+  		myaudio.play();
+  	}
+  })
+
+
+   keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 69){
+  		var myaudio = new Audio('E.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 82){
+  		var myaudio = new Audio('F.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 50){
+  		var myaudio = new Audio('F_s.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 84){
+  		var myaudio = new Audio('G.wav');
+  		myaudio.play();
+  	}
+  })
+
+   keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 51){
+  		var myaudio = new Audio('G_s.wav');
+  		myaudio.play();
+  	}
+  })  
+
+   keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 89){
+  		var myaudio = new Audio('A.wav');
+  		myaudio.play();
+  	}
+  })
+ 
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 85){
+  		var myaudio = new Audio('B.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 52){
+  		var myaudio = new Audio('Bb.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 73){
+  		var myaudio = new Audio('C1.wav');
+  		myaudio.play();
+  	}
+  })
+
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 53){
+  		var myaudio = new Audio('C_s1.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 79){
+  		var myaudio = new Audio('D1.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 54){
+  		var myaudio = new Audio('D_s1.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 80){
+  		var myaudio = new Audio('E1.wav');
+  		myaudio.play();
+  	}
+  })
+  
+   keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 65){
+  		var myaudio = new Audio('F1.wav');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 55){
+  		var myaudio = new Audio('./audios/Piano17.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 83){
+  		var myaudio = new Audio('./audios/Piano120.mp3');
+  		myaudio.play();
+  	}
+  }) 
+ 
+   keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 56){
+  		var myaudio = new Audio('./audios/Piano121.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 68){
+  		var myaudio = new Audio('./audios/Piano110.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 70){
+  		var myaudio = new Audio('./audios/Piano112.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 57){
+  		var myaudio = new Audio('./audios/Piano111.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 71){
+  		var myaudio = new Audio('./audios/Piano125.mp3');
+  		myaudio.play();
+  	}
+  })
+
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 48){
+  		var myaudio = new Audio('./audios/Piano114.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 72){
+  		var myaudio = new Audio('./audios/Piano115.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 189){
+  		var myaudio = new Audio('./audios/Piano116.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 74){
+  		var myaudio = new Audio('./audios/Piano117.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 75){
+  		var myaudio = new Audio('./audios/Piano118.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 187){
+  		var myaudio = new Audio('./audios/Piano119.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 76){
+  		var myaudio = new Audio('./audios/Piano18.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 8){
+  		var myaudio = new Audio('./audios/Piano19.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 67){
+  		var myaudio = new Audio('./audios/Piano122.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 86){
+  		var myaudio = new Audio('./audios/Piano124.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 220){
+  		var myaudio = new Audio('./audios/Piano123.mp3');
+  		myaudio.play();
+  	}
+  })
+
+    keypressed.addEventListener('keydown', (e)=>{
+  	var setnote = e.keyCode;
+  	if(setnote === 66){
+  		var myaudio = new Audio('./audios/Piano113.mp3');
+  		myaudio.play();
+  	}
+  })
+
+ 
